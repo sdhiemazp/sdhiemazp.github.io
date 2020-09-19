5d8778246889cf02fc983e4196fe5678483ef2c7
sdhiemazp <sdhiemazp@gmail.com>
2020-09-19 21:53:51 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,185 @@
+<?php
+    include('pages/layout/header.php');
+?>
+<!--BLOG AREA-->
+<section class="blog-area gray-bg padding-top" id="cokoproduk">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-lg-3 col-sm-6 col-xs-12">
+                <div class="single-blog wow fadeInUp" data-wow-delay="0.2s">
+                    <div class="blog-image">
+                        <img src="img/blog/pribadi.jpg" alt="">
+                    </div>
+                    <div class="blog-details text-center">
+                        <div class="blog-meta"><a href="#"><i class="fa fa-user"></i></a></div>
+                        <h3><a href="pages/order/order_popular1.php.php">Web Pribadi</a></h3>
+                            <h4 class="color-orange">RP. 14.999 / bulan</h4>
+                        <p>Punya website</p>
+                        <p>Tidak punya hosting</p>
+                        <p>Tidak punya domain (URL)</p>
+                        <p class="color-red">*Tidak dengan database</p>
+                        <a href="pages/order/order_popular1.php" class="read-more">COKOKLIK!</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-lg-3 col-sm-6 col-xs-12">
+                <div class="single-blog wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="blog-image">
+                        <img src="img/blog/bisnis.jpg" alt="">
+                    </div>
+                    <div class="blog-details text-center">
+                        <div class="blog-meta"><a href="#"><i class="fa fa-shopping-bag"></i></a></div>
+                        <h3><a href="pages/order/order_popular2.php">Web sd Bisnis</a></h3>
+                            <h4 class="color-orange">RP. 44.999 / bulan</h4>
+                        <p>Punya website</p>
+                        <p>Tidak punya hosting</p>
+                        <p>Tidak punya domain (URL)</p>
+                        <p class="color-green">*Dengan database</p>
+                        <a href="pages/order/order_popular2.php" class="read-more">COKOKLIK!</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-lg-3 col-sm-12 col-xs-12">
+                <div class="single-blog wow fadeInUp" data-wow-delay="0.4s">
+                    <div class="blog-image">
+                        <img src="img/blog/custom.jpg" alt="">
+                    </div>
+                    <div class="blog-details text-center">
+                        <div class="blog-meta"><a href="#"><i class="fa fa-thumbs-o-up"></i></a></div>
+                        <h3><a href="pages/order/order_popular3.php">COKOCustom</a></h3>
+                            <h4 class="color-orange">RP. ???.999 / project</h4>
+                        <p>Web Pribadi</p>
+                        <p>Web bisnis</p>
+                        <p>Dan lain-lain sesuai keinginan</p>
+                        <a href="pages/order/order_popular3.php" class="read-more">COKOKLIK!</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-lg-3 col-sm-6 col-xs-12">
+                <div class="single-blog wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="blog-image">
+                        <img src="img/blog/mockup.jpg" alt="">
+                    </div>
+                    <div class="blog-details text-center">
+                        <div class="blog-meta"><a href="#"><i class="fa fa-tag"></i></a></div>
+                        <h3><a href="pages/order/order_popular4.php">COKOMockUp</a></h3>
+                            <h4 class="color-orange">RP. ???.999 / project</h4>
+                        <p>Punya aplikasi</p>
+                        <p>Tidak punya mockup</p>
+                        <a href="pages/order/order_popular4.php" class="read-more">COKOKLIK!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--BLOG AREA END-->
+
+<!--SERVICE AREA-->
+<section class="service-area" id="cokolayanan">
+    <div class="service-top-area padding-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-lg-6 col-md-offset-3 col-lg-offset-3 col-sm-12 col-xs-12">
+                    <div class="area-title text-center wow fadeIn">
+                        <h2>COKOLAYANAN</h2>
+                        <p>Kasmi siap melayani dan membantu Anda!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12">
+                    <div class="service-content wow fadeIn">
+                        <h2>COKOTITIP adalah solusi!</h2>
+                        <p>Dapat menitipkan web pribadi, web bisnis dan lainnya.<br>Dapat membuatkan web pribadi, web bisnis dan lainnya.<br>Dapat membuatkan mockup project dan lain-lain.</p>
+                        <!-- <a href="service.html" class="read-more">CokoLebih</a> -->
+                    </div>
+                </div>
+                <div class="col-md-8 col-lg-8 col-sm-12 col-xs-12">
+                    <div class="service-catalouge-content-area wow fadeIn">
+                        <div class="service-catalouge-bg"></div>
+                        <div class="row">
+                            <div class="col-md-7 col-lg-7 col-md-offset-5 col-lg-offset-5 col-sm-12 col-xs-12">
+                                <div class="catalouge-content">
+                                    <h3>Kenapa COKOTITIP?</h3>
+                                    <ul>
+                                        <li><i class="fa fa-check"></i> Konsisten</li>
+                                        <li><i class="fa fa-check"></i> Pelayanan Cepat</li>
+                                        <li><i class="fa fa-check"></i> Harga Terjangkau</li>
+                                        <li><i class="fa fa-check"></i> Tepat waktu</li>
+                                    </ul>
+                                    <!-- <a href="service.html" class="read-more">CokoLebih</a> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- <div class="service-bottom-area section-padding">
+        <div class="service-bottom-area-bg"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-lg-6 col-md-offset-6 col-lg-offset-6 col-sm-12 col-xs-12">
+                    <div class="service-list wow fadeIn">
+                        <div class="single-service">
+                            <div class="service-icon-hexagon">
+                                <div class="hex">
+                                    <div class="service-icon">
+                                        <i class="fa fa-dropbox"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="service-details">
+                                <h4>Ware House</h4>
+                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                                <a href="#">read more</a>
+                            </div>
+                        </div>
+                        <div class="single-service">
+                            <div class="service-icon-hexagon">
+                                <div class="hex">
+                                    <div class="service-icon"><i class="fa fa-truck"></i></div>
+                                </div>
+                            </div>
+                            <div class="service-details">
+                                <h4>Road Freight</h4>
+                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                                <a href="#">read more</a>
+                            </div>
+                        </div>
+                        <div class="single-service">
+                            <div class="service-icon-hexagon">
+                                <div class="hex">
+                                    <div class="service-icon"><i class="fa fa-ship"></i></div>
+                                </div>
+                            </div>
+                            <div class="service-details">
+                                <h4>Sea Freight</h4>
+                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                                <a href="#">read more</a>
+                            </div>
+                        </div>
+                        <div class="single-service">
+                            <div class="service-icon-hexagon">
+                                <div class="hex">
+                                    <div class="service-icon"><i class="fa fa-plane"></i></div>
+                                </div>
+                            </div>
+                            <div class="service-details">
+                                <h4>Air Freight</h4>
+                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                                <a href="#">read more</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+</section>
+<!--SERVICE AREA END-->
+<?php
+    include('pages/layout/footer.php');
+?>
